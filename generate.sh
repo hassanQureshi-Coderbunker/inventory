331892bdf5b72817ae6af02f351f6af33ed4b19a
Ricky Ng-Adam <rngadam@gmail.com>
2017-08-10 14:16:39 +0800

    449B1560-1B86-48E5-9AD4-58B83AD06BAB

--- generate.sh
+++ generate.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 PAGEID=`uuidgen`
+echo "Generating batch $PAGEID"
 mkdir -p pages/$PAGEID
 cd pages/$PAGEID
 for i in $(seq 24)
 do
 	export ID=`uuidgen`
 	SHORTID=`echo $ID | cut -d '-' -f 1`
+	echo "$SHORTID $ID" >> index.txt
 	mkdir -p full/
 	mkdir -p short/
-	qrencode http://url.coderbunker.com/`uuidgen` -o full/$ID.png
-	convert full/$ID.png  label:"$SHORTID" -gravity Center -append short/$SHORTID.png
+	qrencode -s 10 http://url.coderbunker.com/`uuidgen` -o full/$ID.png
+	convert -pointsize 60 full/$ID.png label:"$SHORTID" -gravity Center -append short/$SHORTID.png
 done
-montage short/*.png -tile 4x6 page.png
+montage -mode concatenate short/*.png -tile 4x6 page.png
